2cbe8d5ba5f3f8840d9f64af0cc1fe79ef8e5b8b
obai-hup <obai2015@yahoo.com>
2022-01-10 20:20:50 +0200

    First Initial

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _db;
+
+        public UsersController(DataContext db)
+        {
+            _db = db;
+        }
+
+        /*
+        ***
+        ** Get All Users 
+        ** api/GetUsers
+        ***
+        */
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _db.Users.ToListAsync();
+        }
+
+        /*
+        ***
+        ** Get One User By Id
+        ** api/GetUser/id
+        ***
+        */
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _db.Users.FindAsync(id);
+        }
+    }
+}
